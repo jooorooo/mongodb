89150406add69a6dbb1a3762e9252a5669187c70
Georgi Nachev <g.nachev@webmedia.bg>
2019-03-05 08:59:46 +0200

    up

--- src/Simexis/Mongodb/Relations/HasOne.php
+++ src/Simexis/Mongodb/Relations/HasOne.php
@@ -3,6 +3,7 @@
 namespace Simexis\Mongodb\Relations;
 
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Relations\HasOne as EloquentHasOne;
 use Illuminate\Database\Eloquent\Model as EloquentModel;
 
@@ -91,4 +92,47 @@ class HasOne extends EloquentHasOne
     {
         return 'whereIn';
     }
+
+    /**
+     * Match the eagerly loaded results to their many parents.
+     *
+     * @param  array   $models
+     * @param  \Illuminate\Database\Eloquent\Collection  $results
+     * @param  string  $relation
+     * @param  string  $type
+     * @return array
+     */
+    protected function matchOneOrMany(array $models, Collection $results, $relation, $type)
+    {
+        $dictionary = $this->buildDictionary($results);
+
+        // Once we have the dictionary we can simply spin through the parent models to
+        // link them up with their children using the keyed dictionary to make the
+        // matching very convenient and easy work. Then we'll just return them.
+        foreach ($models as $model) {
+            if (isset($dictionary[$key = $this->getQuery()->objectIdToString($model->getAttribute($this->localKey))])) {
+                $model->setRelation(
+                    $relation, $this->getRelationValue($dictionary, $key, $type)
+                );
+            }
+        }
+
+        return $models;
+    }
+
+    /**
+     * Build model dictionary keyed by the relation's foreign key.
+     *
+     * @param  \Illuminate\Database\Eloquent\Collection  $results
+     * @return array
+     */
+    protected function buildDictionary(Collection $results)
+    {
+        $foreign = $this->getForeignKeyName();
+
+        return $results->mapToDictionary(function ($result) use ($foreign) {
+            return [$this->getQuery()->objectIdToString($result->{$foreign}) => $result];
+        })->all();
+    }
+
 }
